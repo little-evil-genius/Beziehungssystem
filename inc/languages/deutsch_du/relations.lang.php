c5f4a2ecbf71b2a8911bb8974f941348e646d958
Lara <75126217+little-evil-genius@users.noreply.github.com>
2021-06-11 21:47:39 +0200

    Create relations.lang.php

---
+++ inc/languages/deutsch_du/relations.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$l['relations_new'] = "{1} hat dich zu seiner Beziehungskiste hinzugefügt!";
+$l['myalerts_setting_relations_new'] = "Benachrichtigung bei neuer Beziehungseintragung?";
+
+$l['relations_alert_edit'] = "{1} den Beziehungseintrag verändert!";
+$l['myalerts_setting_relations_alert_edit'] = "Benachrichtigung bei Veränderungen vom Beziehungseintragung?";
+
+$l['relations_delete'] = "{1} den Beziehungseintrag gelöscht!";
+$l['myalerts_setting_relations_delete'] = "Benachrichtigung bei Löschungen vom Beziehungseintragung?";
+
+?>
