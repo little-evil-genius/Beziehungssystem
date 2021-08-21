acf753367c0be33d3138d83f5769e9745e2e931e
Lara <75126217+little-evil-genius@users.noreply.github.com>
2021-08-21 21:27:46 +0200

    Create relations.lang.php

---
+++ inc/languages/deutsch_sie/relations.lang.php
@@ -0,0 +1,35 @@
+<?php
+$l['relations'] = 'Beziehungen';
+$l['relations_add'] = 'Zur Beziehungskiste hinzufügen';
+$l['relations_add_relationship'] = 'Kurzes Schlagwort über die Beziehung der beiden Charaktere?';
+$l['relations_add_desc'] = 'Ausführliche Beschreibung der Beziehung zwischen den beiden Charaktere?';
+$l['relations_add_send'] = 'Hinzufügen zur Beziehungskiste';
+
+$l['relations_add_npc'] = 'NPC zur Beziehungskiste hinzufügen';
+$l['relations_add_npc_name'] = 'Vorname Nachname';
+$l['relations_add_npc_info'] = 'XXX Jahre | Beziehungsstatus';
+$l['relations_add_npc_search'] = 'Gibt es ein Gesuch zu diesem NPC? Hier den Link angeben';
+$l['relations_add_npc_send'] = 'NPC hinzufügen zur Beziehungskiste';
+
+$l['relations_edit_send'] = 'Beziehungseintrag editieren';
+$l['relations_edit_npc_send'] = 'NPC editieren';
+
+$l['relations_none'] = 'Es ist keine Beziehung innerhalb dieser Kategorie vorhanden!';
+
+$l['relations_redirect_add'] = 'Der neue Beziehungseintrag wurde erfolgreich zu deiner Beziehungskiste hinzugefügt!';
+$l['relations_redirect_add_npc'] = 'Der neue NPC wurde erfolgreich zu deiner Beziehungskiste hinzugefügt!';
+$l['relations_redirect_edit'] = "Du hast den Beziehungseintrag erfolgreich bearbeitet und wirst nun zurück auf dein Profil geleitet!";
+$l['relations_redirect_edit_npc'] = "Du hast den NPC erfolgreich bearbeitet und wirst nun zurück auf dein Profil geleitet!";
+$l['relations_redirect_delete'] = "Du hast den Beziehungseintrag erfolgreich aus deiner Beziehungskiste gelöscht und wirst nun zurück auf dein Profil geleitet!";
+
+// MyALERTS
+$l['relations_new'] = '{1} hat dich als <strong>"{4}"</strong> in die Kategorie <strong>"{5}"</strong> bei sich in die Beziehungskiste eingetragen!';
+$l['myalerts_setting_relations_new'] = "Benachrichtigung bei neuer Beziehungseintragung?";
+
+$l['relations_alert_edit'] = '{1} den Beziehungseintrag zu <strong>"{4}"</strong> in der Kategorie <strong>"{5}"</strong> verändert!';
+$l['myalerts_setting_relations_alert_edit'] = "Benachrichtigung bei Veränderungen vom Beziehungseintragung?";
+
+$l['relations_delete'] = '{1} den Beziehungseintrag <strong>"{4}"</strong> in der Kategorie <strong>"{5}"</strong> gelöscht!';
+$l['myalerts_setting_relations_delete'] = "Benachrichtigung bei Löschungen vom Beziehungseintragung?";
+
+?>
